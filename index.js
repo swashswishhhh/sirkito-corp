2ceac6bf862264cad23cb4c1d02fa73d1c8d8e63
swashswishhhh <swashswish47@gmail.com>
2026-03-06 22:01:48 +0800

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-// Entry file to expose the main Home page component at the project root.
-// It simply reuses everything from `app/page.tsx`.
-
-import Home from "./sirkito-company/app/page";
-
-export default Home;
-
