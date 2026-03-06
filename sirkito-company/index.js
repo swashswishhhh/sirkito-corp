ed6d747a74ac49474886e55d7eaff24e827be0ca
swashswishhhh <swashswishhhh@gmail.com>
2026-03-06 20:45:29 +0800

    Add index.js

---
+++ sirkito-company/index.js
@@ -0,0 +1,7 @@
+// Entry file to expose the main Home page component at the project root.
+// It simply reuses everything from `app/page.tsx`.
+
+import Home from "./app/page";
+
+export default Home;
+
